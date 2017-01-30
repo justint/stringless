3cbaaaea5e73ca25d165a4aaada178f8d4d8678f
Justin Tennant <justin.tennant@sjsu.edu>
2017-01-29 17:17:35 -0800

    Create functioning shared memory w deletion arg, include links in README

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Important links:
+[Boost shared memory](http://www.boost.org/doc/libs/1_54_0/doc/html/interprocess/sharedmemorybetweenprocesses.html)
+[Boost synchronization](http://www.boost.org/doc/libs/1_58_0/doc/html/thread/synchronization.html)
--- src/welcome.cc
+++ src/welcome.cc
@@ -1,3 +1,8 @@
+// cs180_shared_memory_test
+// 
+// Practice with shared memory in linux environment, using Boost
+// Run with arg '1' (without quotes) to remove shared memory
+
 #include <iostream>
 
 #include <boost/interprocess/shared_memory_object.hpp>
@@ -7,15 +12,37 @@
 
 int main(int argc, char**argv) {
     
-    boost::interprocess::shared_memory_object shm_obj
-            (boost::interprocess::open_or_create, 
-            "shared_memory", 
-            boost::interprocess::read_only);
+    using namespace boost::interprocess;
+
+    if (atoi(argv[1]) == 1)
+    {
+        shared_memory_object::remove("shared_memory");
+        std::cout << "Successfully removed shared memory" << std::endl;
+    }
+    else {
+        const offset_t truncate_length = 100;
+
+        shared_memory_object shm_obj
+                    (create_only,
+                    "shared_memory", 
+                    read_write);
+
+        shm_obj.truncate(truncate_length);    
+
+        std::cout << "Successfully truncated shared memory to size " << 
+                truncate_length << std::endl;
+
+        size_t shm_size = sizeof(truncate_length);
+
+        // Create mapped region of whole shared memory
+        mapped_region region(shm_obj, read_write);
+
+        std::cout << region.get_address() << std::endl;
+        std::cout << region.get_size() << std::endl;
+
+        // Write all memory to 1(?)
+        std::memset(region.get_address(), 1234, region.get_size());
+    }
     
-    std::cout << "Successfully created + opened shared memory" << std::endl;
-    
-    boost::interprocess::shared_memory_object::remove("shared_memory");
-    
-    std::cout << "Successfully removed shared memory" << std::endl;
     return 0;
 }
