f6dc355ffbe1a8f7af69966a47b8a6aec8e86389
Justin Tennant <justin.tennant@sjsu.edu>
2017-01-30 16:12:55 -0800

    Dropped boost and switched to C-style pthread/mutex shared memory
    So the code in welcome.cc still has my boost code, I've left it alone
    and started semi-fresh in welcome_c.cc. In that new file, I've
    successfully gotten C-style (shm_open(), mmap(), etc) to work. I added
    pthread mutexes to both read and write, but not sure if that's actually
    properly locking the shared memory.
    
    I wrote sample code that writes "Hello world!" and an integer counting 
    up incrementally every second to the shared memory, and a reader that 
    reads the memory content and prints it every second as well.
    
    Maybe I'll stick with this? If I have time, I'll look into Qt's shared
    memory functionalities and see if that can work the way I'd like it to.

--- src/welcome.cc
+++ src/welcome.cc
@@ -4,9 +4,15 @@
 // Run with arg '1' (without quotes) to remove shared memory, 2 to read it
 
 #include <iostream>
+#include <mutex>
 
 #include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/sync/named_mutex.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
 #include <boost/interprocess/mapped_region.hpp>
+
+#include <unistd.h>
 
 #include "welcome.h"
 
@@ -30,31 +36,59 @@ int main(int argc, char**argv) {
             char *mem = static_cast<char*>(region.get_address());
             for (auto i = 0; i < region.get_size(); ++i)
             {
-                std::cout << *mem++ << std::endl;
+                std::cout << static_cast<int>(*mem++) << std::endl;
             }
         }
     }
     else {
-        const offset_t truncate_length = 10;
+        const offset_t memory_size = 2048;
 
+        managed_shared_memory managed_shm(open_or_create, "shared_memory", memory_size);
+        
+        //Allocate 100 bytes of memory from segment, throwing version
+         void *ptr = managed_shm.allocate(10);
+         
+         // Create the named mutex
+         named_mutex mutex(open_or_create, "shared_memory_mutex");
+         
+         std::mutex mtx;
+         
+         
+            // Lock the mutex
+            //scoped_lock<named_mutex> lock(mutex);
+            //mutex.lock();
+            //std::memset(managed_shm.get_address(), 1, managed_shm.get_size());
+            mtx.lock();
+            std::cout << "Hello" << std::endl;
+            mtx.unlock();
+            //sleep(10);
+            //mutex.unlock();
+         
+         
+         
+         //Deallocate it
+         //managed_shm.deallocate(ptr);
+
+        
+        /*
         shared_memory_object shm_obj
-                    (create_only,
+                    (open_or_create,
                     "shared_memory", 
                     read_write);
-
+        
         shm_obj.truncate(truncate_length);    
 
         std::cout << "Successfully truncated shared memory to size " << 
                 truncate_length << std::endl;
-
-        size_t shm_size = sizeof(truncate_length);
 
         // Create mapped region of whole shared memory
         mapped_region region(shm_obj, read_write);
 
         std::cout << region.get_address() << std::endl;
         std::cout << region.get_size() << std::endl;
-
+        
+        */
+        
         // Set all memory to 1 (works!)
         //std::memset(region.get_address(), 1, region.get_size());
         
@@ -73,6 +107,16 @@ int main(int argc, char**argv) {
         std::memcpy(region.get_address(), test, region.get_size());
          */
         
+        
+        // Mutex test - will write for some time while I try to read from
+        // another process.
+        /*
+        std::cout << "Beginning mutex lock..." << std::endl;
+        named_mtx.lock();
+            std::memset(region.get_address(), 1, region.get_size());
+        named_mtx.unlock();
+        std::cout << "Mutex unlocked" << std::endl;
+         */
     }
     
     return 0;
