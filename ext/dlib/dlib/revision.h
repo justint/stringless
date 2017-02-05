c5d8b593c5d3b35bb333338941a26583b570047c
Justin Tennant <justin.tennant@sjsu.edu>
2017-02-05 00:50:46 -0800

    Include local instance of dlib
    Adding the local instance of dlib allows me to compile it with
    Stringless, and by adding the -DUSE_AVX_INSTRUCTIONS=ON cmake option,
    I can get crazy good speeds out of dlib's face/face landmark detection.

---
+++ ext/dlib/dlib/revision.h
@@ -0,0 +1,8 @@
+#ifndef DLIB_REVISION_H
+// Version:  19.2
+// Date:     Mon Oct 10 19:34:36 EDT 2016
+// Mercurial Revision ID:  0e6647d222c3
+#define DLIB_MAJOR_VERSION  19
+#define DLIB_MINOR_VERSION  2
+#define DLIB_PATCH_VERSION  0
+#endif
